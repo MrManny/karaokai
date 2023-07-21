24a1a0ad93e7cd647f1712e0bf617aa19281ae85
Manuel Warum <mwarum@gmail.com>
2023-07-21 15:22:53 +0200

    #7: Use Tailwind CSS for styling

---
+++ src/components/brand/Brand.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>
+    Karaok<span class="brand-suffix">AI</span>
+  </h1>
+</template>
+
+<style>
+.brand-suffix {
+  @apply font-semibold text-berkeley;
+}
+
+@media screen and (prefers-color-scheme: dark) {
+  .brand-suffix {
+    @apply text-cerulean;
+  }
+}
+</style>
