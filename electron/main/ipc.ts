55c39ea81cd9497e44269cecda3914acb779d6fd
Manuel Warum <mwarum@gmail.com>
2023-07-22 12:09:38 +0200

    #3: Extract IPC and path data into separate file

---
+++ electron/main/ipc.ts
@@ -0,0 +1,19 @@
+// New window example arg: new windows url
+import { BrowserWindow, ipcMain } from 'electron';
+import { indexHtml, preload, url } from './paths';
+
+ipcMain.handle('open-win', (_, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    void childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    void childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
