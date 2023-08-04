5604a9a18c9d889f51cf085fa4fc3b8cb8bfec0c
Manuel Warum <mwarum@gmail.com>
2023-08-04 19:00:45 +0200

    #38: Add coverage reporting (#40)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,12 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   test: {
+    coverage: {
+      all: true,
+      provider: 'istanbul', // not constantinople
+      reporter: ['text', 'json-summary', 'json'],
+      include: ['src/**/*.ts', 'src/**/*.vue'],
+    },
     environment: 'jsdom',
   },
 });
